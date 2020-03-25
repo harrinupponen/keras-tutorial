371f3404b546654adbae20791a3954d5ee00719c
Harri Nupponen <harrinupponen@gmail.com>
2020-03-25 18:23:33 +0200

    Add and preprocess data, MenWomenCNN.py

---
+++ MenWomenCNN.py
@@ -0,0 +1,95 @@
+import numpy as np
+import keras
+from keras.models import Sequential
+from keras.layers import Activation
+from keras.layers.core import Dense, Flatten
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import *
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+train_path = 'men_women/train'
+valid_path = 'men_women/valid'
+test_path = 'men_women/test'
+
+train_batches = ImageDataGenerator().flow_from_directory(train_path, target_size=(224, 224), classes=['man', 'woman'], batch_size=10)
+valid_batches = ImageDataGenerator().flow_from_directory(valid_path, target_size=(224, 224), classes=['man', 'woman'], batch_size=4)
+test_batches = ImageDataGenerator().flow_from_directory(test_path, target_size=(224, 224), classes=['man', 'woman'], batch_size=10)
+
+# plots images with labels  (Remember to pip install pillow)
+def plots(ims, figsize=(12, 8), rows=1, interp=False, titles=None):
+    if type(ims[0]) is np.ndarray:
+        ims = np.array(ims).astype(np.uint8)
+        if(ims.shape[-1] != 3):
+            ims = ims.transpose((0,2,3,1))
+    f = plt.figure(figsize=figsize)
+    cols = len(ims)//rows if len(ims) % 2 == 0 else len(ims)//rows + 1
+    for i in range(len(ims)):
+        sp = f.add_subplot(rows, cols, i + 1)
+        sp.axis('Off')
+        if titles is not None:
+            sp.set_title(titles[i], fontsize=16)
+        plt.imshow(ims[i], interpolation=None if interp else 'none')
+
+imgs, labels = next(train_batches)
+plots(imgs, titles=labels)
+#plt.show()
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)),
+    Flatten(),
+    Dense(2, activation='softmax')
+])
+
+model.compile(Adam(lr=.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit_generator(train_batches, steps_per_epoch=10, validation_data=valid_batches,
+                    validation_steps=10, epochs=5, verbose=2)
+
+
+test_images, test_labels = next(test_batches)
+#plots(test_images, titles=test_labels)
+#plt.show()
+#print(test_labels)
+predictions = model.predict_generator(test_batches, steps=1, verbose=0)
+#print(predictions)
+
+cm = confusion_matrix(test_labels, predictions[:,0])
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+
+    """This fn prints and plots the confusion matrix. Normalization can be applied by setting 'normalize=True'."""
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print('Normalized confusion matrix')
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                horizontalalignment='center',
+                color='white' if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+#----------------- Use Confusion Matrix---------------------
+
+cm_plot_labels = ['man', 'woman']
+plot_confusion_matrix(cm, cm_plot_labels, title='Confusion Matrix')
+plt.show()
